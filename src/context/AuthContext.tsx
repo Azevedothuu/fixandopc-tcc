bc5e033931b89429c28f99f10cf96caa59ea39ab
Azevedothuu <gamesazevedo01@gmail.com>
2025-12-03 14:47:25 -0300

    fix value is never read

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import type { ReactNode } from "react";
 import { loginApi, registerApi } from "../api/apiClient";
 import type { LoginRequest, RegisterRequest } from "../types/auth";
