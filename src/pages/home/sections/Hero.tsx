e5d82f096aee7d0d76c7a8752a3c64ed501b95ba
Amauri <blancoamauri@gmail.com>
2025-12-03 19:59:40 -0300

    hero

--- src/pages/home/sections/Hero.tsx
+++ src/pages/home/sections/Hero.tsx
@@ -1,38 +1,33 @@
-import type { JSX } from "react"
-import Logo from "@assets/Logo+Letreiro.png"
-import { Typography } from "@componentes/typography"
-import { ButtonCSS } from "@/components/ButtonCSS"
-
+import type { JSX } from "react";
+import Logo from "@assets/Logo+Letreiro.webp";
+import { Typography } from "@componentes/typography";
+import { ButtonCSS } from "@/components/ButtonCSS";
 
 export function Hero(): JSX.Element {
-
-
-  return (
-    <>
-      <section id="Hero">
-        <div className="w-full h-screen flex justify-end">
-          <div className="items-center flex">
-            <img src={Logo} className="w-88" />
-          </div>
-
-          <div className="flex flex-col justify-center gap-4 p-4 text-primary-300">
-            <div className="flex w-auto ">
-              <Typography size="xs" className="leading-relaxed pr-20" as="p">
-                Um espaço seguro e colaborativo criado para democratizar o
-                <br />
-                suporte técnico, ajudando qualquer pessoa a entender, <br />
-                cuidar e resolver problemas no próprio computador.
-              </Typography>
-            </div>
-
-            <div className="flex justify-center ">
-              <ButtonCSS
-              onClick={() => window.location.hash = "sobrenos"} 
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  )
+    return (
+        <>
+            <section id="Hero" className="container mx-auto">
+                <div className="w-full h-screen flex justify-end">
+                    <div className="flex w-auto flex-col items-center justify-center gap-8 lg:pr-20">
+                        <img src={Logo} className="w-50" />
+                        <Typography
+                            size="heading-md"
+                            className="leading-relaxed text-lg text-center text-primary-400"
+                            as="p"
+                        >
+                            Um espaço seguro e colaborativo criado para
+                            democratizar o
+                            <br />
+                            suporte técnico, ajudando qualquer pessoa a
+                            entender, <br />
+                            cuidar e resolver problemas no próprio computador.
+                        </Typography>
+                        <ButtonCSS
+                            onClick={() => (window.location.hash = "sobrenos")}
+                        />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
